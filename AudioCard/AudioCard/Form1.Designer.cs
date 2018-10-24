30a70a503674f5133c1c78d9a76a18a46e8ed44c
blonski <piotrblonski96@gmail.com>
2018-10-24 16:16:20 +0200

    Audio card - mci

--- AudioCard/AudioCard/Form1.Designer.cs
+++ AudioCard/AudioCard/Form1.Designer.cs
@@ -35,6 +35,12 @@
             this.DirectSound_lb = new System.Windows.Forms.Label();
             this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
             this.MediaPlayer_lb = new System.Windows.Forms.Label();
+            this.startMicro = new System.Windows.Forms.Button();
+            this.endMicro = new System.Windows.Forms.Button();
+            this.MciPlayBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
             this.SuspendLayout();
             // 
@@ -73,9 +79,9 @@
             this.DirectSound_lb.AutoSize = true;
             this.DirectSound_lb.Location = new System.Drawing.Point(12, 25);
             this.DirectSound_lb.Name = "DirectSound_lb";
-            this.DirectSound_lb.Size = new System.Drawing.Size(69, 13);
+            this.DirectSound_lb.Size = new System.Drawing.Size(67, 13);
             this.DirectSound_lb.TabIndex = 3;
-            this.DirectSound_lb.Text = "Direct Sound";
+            this.DirectSound_lb.Text = "SoundPlayer";
             // 
             // mediaPlayer
             // 
@@ -96,11 +102,77 @@
             this.MediaPlayer_lb.TabIndex = 5;
             this.MediaPlayer_lb.Text = "Media Player";
             // 
+            // startMicro
+            // 
+            this.startMicro.Location = new System.Drawing.Point(395, 41);
+            this.startMicro.Name = "startMicro";
+            this.startMicro.Size = new System.Drawing.Size(75, 23);
+            this.startMicro.TabIndex = 6;
+            this.startMicro.Text = "Nagrywaj";
+            this.startMicro.UseVisualStyleBackColor = true;
+            this.startMicro.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // endMicro
+            // 
+            this.endMicro.Location = new System.Drawing.Point(395, 70);
+            this.endMicro.Name = "endMicro";
+            this.endMicro.Size = new System.Drawing.Size(75, 23);
+            this.endMicro.TabIndex = 7;
+            this.endMicro.Text = "Zakończ";
+            this.endMicro.UseVisualStyleBackColor = true;
+            this.endMicro.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MciPlayBtn
+            // 
+            this.MciPlayBtn.Location = new System.Drawing.Point(382, 186);
+            this.MciPlayBtn.Name = "MciPlayBtn";
+            this.MciPlayBtn.Size = new System.Drawing.Size(108, 23);
+            this.MciPlayBtn.TabIndex = 8;
+            this.MciPlayBtn.Text = "playSound Mci";
+            this.MciPlayBtn.UseVisualStyleBackColor = true;
+            this.MciPlayBtn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Resume";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(395, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MciPlayBtn);
+            this.Controls.Add(this.endMicro);
+            this.Controls.Add(this.startMicro);
             this.Controls.Add(this.MediaPlayer_lb);
             this.Controls.Add(this.mediaPlayer);
             this.Controls.Add(this.DirectSound_lb);
@@ -124,6 +196,12 @@
         private System.Windows.Forms.Label DirectSound_lb;
         private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
         private System.Windows.Forms.Label MediaPlayer_lb;
+        private System.Windows.Forms.Button startMicro;
+        private System.Windows.Forms.Button endMicro;
+        private System.Windows.Forms.Button MciPlayBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
